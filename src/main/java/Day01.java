f08f3991a049528cdb8fdacb5ea0406177111b0f
Ryan Christilaw <ryan.christilaw@gmail.com>
2023-12-01 21:02:19 -0500

    Day 1

--- src/main/java/Day01.java
+++ src/main/java/Day01.java
@@ -2,83 +2,132 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.util.ArrayList;
-import java.util.stream.Collectors;
+import java.util.List;
+import java.util.Map;
 
 @Slf4j
 public class Day01 extends BaseDay {
-    private final static String inputFile = "day01.txt";
+
+    private static final Map<String, String> numbers = Map.of(
+            "one", "1",
+            "two", "2",
+            "three", "3",
+            "four", "4",
+            "five", "5",
+            "six", "6",
+            "seven", "7",
+            "eight", "8",
+            "nine", "9"
+    );
 
     public static void main(String... args) throws URISyntaxException, IOException {
-        final var day1 = new Day01();
-        day1.runPart1();
-        day1.runPart2();
+        final var day = new Day01();
+
+        final var input = getInput(day.getClass(), false);
+        day.run(input);
     }
 
-    public void runPart1() throws URISyntaxException, IOException {
-        final var input = readClassPathResource(inputFile);
-        final var calorieReadings = input.lines()
-            .map(it -> {
-                try {
-                    return Integer.valueOf(it);
-                } catch (Exception exception) {
-                    return null;
-                }
-            })
-            .toList();
+    public void run(String input) {
 
-        final var sums = new ArrayList<Integer>();
+        final var rows = input.lines()
+                .map(it -> it.split(""))
+                .toList();
 
-        var currentSum = 0;
-        for (final var reading : calorieReadings) {
-            if (reading == null) {
-                sums.add(currentSum);
-                currentSum = 0;
-                continue;
-            }
-            currentSum += reading;
+        int sum = 0;
+
+        for (final var row : rows) {
+            final var first = findNumber(row, false);
+            final var last = findNumber(row, true);
+
+            final var num = first + last;
+            log.info(num);
+            sum += Integer.parseInt(num);
         }
+        
+        log.info("Part 1 solution: {}", sum);
 
-        final var orderedSums = sums.stream()
-            .sorted()
-            .toList();
 
-        log.info("Results: {}", orderedSums);
+        final var rows2 = input.lines().toList();
+
+        int sum2 = 0;
+
+        for (final var row : rows2) {
+            final var first = findFirstNumber(row);
+            final var last = findLastNumber(row);
+
+            final var num = first + last;
+            log.info(first);
+            log.info(last);
+
+            log.info(num);
+            sum2 += Integer.parseInt(num);
+        }
+        log.info("Part 2 solution: {}", sum2);
     }
 
-    public void runPart2() throws URISyntaxException, IOException {
-        final var input = readClassPathResource(inputFile);
-        final var calorieReadings = input.lines()
-            .map(it -> {
-                try {
-                    return Integer.valueOf(it);
-                } catch (Exception exception) {
-                    return null;
-                }
-            })
-            .toList();
-
-        final var sums = new ArrayList<Integer>();
-
-        var currentSum = 0;
-        for (final var reading : calorieReadings) {
-            if (reading == null) {
-                sums.add(currentSum);
-                currentSum = 0;
-                continue;
+    private String findFirstNumber(String val) {
+        for (int i = 0; i < val.length(); i++) {
+            final var c = val.substring(i, i + 1);
+            if (isNumber(c)) {
+                return c;
             }
-            currentSum += reading;
+            final var num = getStringNum(val.substring(i, val.length() - 1));
+            if (num != null) {
+                return num;
+            }
+        }
+        return null;
+    }
+
+    private String getStringNum(String val) {
+        for (var numString : numbers.keySet()) {
+            if (val.indexOf(numString) == 0) {
+                return numbers.get(numString);
+            }
         }
 
-        final var orderedMonkeys = sums.stream()
-            .sorted()
-            .toList();
+        return null;
+    }
 
-        final var numberOfMonkeys = orderedMonkeys.size();
-        final var topMonkeys = orderedMonkeys.get(numberOfMonkeys - 1)
-            + orderedMonkeys.get(numberOfMonkeys - 2)
-            + orderedMonkeys.get(numberOfMonkeys - 3);
+    private String findLastNumber(String val) {
+        for (int i = val.length() - 1; i >= 0; i--) {
+            final var c = val.substring(i, i + 1);
+            if (isNumber(c)) {
+                return c;
+            }
+            final var num = getStringNum(val.substring(i, val.length()));
+            if (num != null) {
+                return num;
+            }
+        }
+        return null;
+    }
 
-        log.info("Results: {}", topMonkeys);
+
+    private String findNumber(String[] list, boolean reverse) {
+        if (reverse) {
+            for (int i = list.length - 1; i >= 0; i--) {
+                if (isNumber(list[i])) {
+                    return list[i];
+                }
+            }
+        } else {
+            for (final var val : list) {
+                if (isNumber(val)) {
+                    return val;
+                }
+            }
+        }
+        return null;
+    }
+
+
+    private boolean isNumber(String val) {
+        try {
+            Integer.parseInt(val);
+            return true;
+        } catch (Exception ex) {
+            return false;
+        }
     }
 }
